0e67d489e1d2dc095f0f21899f9ce354b10ac949
oerreo <97029092+oerreo@users.noreply.github.com>
2024-06-08 16:44:53 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240608/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for(int i = 0; i < 4; i++)
+            print5Stars(); 
+    }
+
+    public static void print5Stars() {
+        System.out.pirntln("*****");
+}
+
+}
